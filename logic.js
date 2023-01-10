f0ee03cadbd5734b0bb3b51dedd22cd5fa4eb7bb
monica-reverte <monicareverte24@gmail.com>
2023-01-10 15:27:48 +0100

    new file alert js

--- logic.js
+++ logic.js
@@ -13,3 +13,24 @@ fetch('index.php', {
     // manejar la respuesta del servidor
 })
 .catch(error => console.error(error));
+
+//New Folder
+
+newButton = document.querySelector(".newButton");
+newButton.addEventListener("click", newFile);
+
+function newFile() {
+    const filename = prompt("Enter name");
+
+    if(filename) {
+        post("create.php", {filename, dir}, function(data) {
+        if(data == true) {
+            openFolder(dir);
+        }
+        else {
+            console.log(data);
+        }
+        closeMenu();
+        });
+    }
+}
