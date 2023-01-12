8831cb3b04d61ca9c3a012628e3bf7e36ebaee71
Edgar Arcos <edgararcosmartin@gmail.com>
2023-01-12 10:03:31 +0000

    buscador en proceso

--- busqueda.php
+++ busqueda.php
@@ -2,7 +2,7 @@
 		$fileName = $_POST["busquedas"];
         
         if (file_exists($fileName)) {
-            echo "el fichero existe";
+            echo "el fichero" .$fileName. "existe";
         } else {
             echo "el fichero no existe";
         }
